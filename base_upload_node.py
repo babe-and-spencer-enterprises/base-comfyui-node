c8120d47db2d0c8f3ada16544e50608f79406eb9
srallyn <spencerallyn@gmail.com>
2025-05-13 13:37:16 -0700

    Switch to png

--- base_upload_node.py
+++ base_upload_node.py
@@ -47,7 +47,7 @@ class UploadToBaseNode:
 
         img = Image.fromarray(img_array)
         buffer = BytesIO()
-        img.save(buffer, format="JPEG")
+        img.save(buffer, format="PNG")
         b64_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
 
         response = requests.post("https://us-central1-base-14bf3.cloudfunctions.net/uploadImageToBase", json={
